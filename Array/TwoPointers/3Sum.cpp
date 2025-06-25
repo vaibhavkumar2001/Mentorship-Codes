97bbfc5eb94615db37cc96fae30fc2d2462ad9a9
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-06-25 15:34:21 +0530

    Implement threeSum function to find all unique triplets in an array that sum to zero

---
+++ Array/TwoPointers/3Sum.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end()); // Sort the array
+
+        for (int i = 0; i < n; i++) {
+            // Skip duplicate elements
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    // Skip duplicates
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Sample main function to test
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = sol.threeSum(nums);
+
+    for (const auto& triplet : result) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
